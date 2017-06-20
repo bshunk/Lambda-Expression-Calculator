c4abdcddacbf61d3acd9421e6d2f95bb51cc77ae
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-20 13:03:43 -0500

    create index and JS and begin work on calculator challenge

---
+++ calculator.js
@@ -0,0 +1,42 @@
+console.log("Ahoy, Captain!");
+
+
+// document.getElementById("addBtn", "subBtn", "mulBtn", "divBtn").addEventListener("click", function(){
+//     document.getElementById("firstNum", "secondNum").innerHTML = "Your result!";
+// });
+
+document.getElementById("addBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+};
+
+
+
+
+
+
+
+
+
+// Create a function that multiplies two numbers passed in as arguments. Return the product.
+
+
+
+// Create a function that adds two numbers passed in as arguments. Return the sum.
+
+
+
+// Create a function that subtracts two numbers passed in as arguments. Return the difference.
+
+
+
+// Create a function that divides two numbers passed in as arguments. Return the quotient.
+
+
+
+// Create a function that accepts three arguments.
+    // 1. First number
+    // 2. Second number
+    // 3. A function that performs an operation on them
+// Return the value of the operation.
