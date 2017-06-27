b0956acf528375970aa23e4c580a2d13e2660831
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-27 10:43:32 -0500

    work on using callbacks nd iifes on calc expression

--- calculator.js
+++ calculator.js
@@ -8,44 +8,47 @@ console.log("Ahoy, Captain!");
 // 	console.log(piggie, piggie.classList)
 // });
 
+var operations = (function(firstNum, secondNum) { 
 
-document.getElementById("mulBtn").addEventListener("click", function() {
-	let multiply = function (firstNum, secondNum) {
-		return firstNum * secondNum;
-		var product = firstNum * secondNum; 
-		console.log("product", event.target);
-	}
-});
+	document.getElementById("mulBtn").addEventListener("click", function() {
+		let multiply = function (callback) {
+			return firstNum * secondNum;
+			var product = firstNum * secondNum; 
+			console.log("product", event.target);
+		}
+	});
 
-// Create a function that adds two numbers passed in as arguments. Return the sum.
+	// Create a function that adds two numbers passed in as arguments. Return the sum.
 
-// document.getElementById("addBtn").addEventListener("click", addition);
+	document.getElementById("addBtn").addEventListener("click", addition);
 
-// var addition = function (firstNum, secondNum) {
-//     return firstNum + secondNum;
-//     console.log(addition);
-// };
+	var addition = function (callback) {
+	    return firstNum + secondNum;
+	    console.log(addition);
+	};
 
-// // Create a function that subtracts two numbers passed in as arguments. Return the difference.
+	// Create a function that subtracts two numbers passed in as arguments. Return the difference.
 
-// document.getElementById("subBtn").addEventListener("click", subtraction);
+	document.getElementById("subBtn").addEventListener("click", subtraction);
 
-// var subtraction = function (firstNum, secondNum) {
-//     return firstNum - secondNum;
-//     console.log(subtraction);
-// };
+	var subtraction = function (callback) {
+	    return firstNum - secondNum;
+	    console.log(subtraction);
+	};
 
-// // Create a function that divides two numbers passed in as arguments. Return the quotient.
+	// Create a function that divides two numbers passed in as arguments. Return the quotient.
 
-// document.getElementById("divBtn").addEventListener("click", division);
+	document.getElementById("divBtn").addEventListener("click", division);
 
-// var division = function (firstNum, secondNum) {
-//     return firstNum / secondNum;
-//     console.log(division);
-// };
+	var division = function (callback) {
+	    return firstNum / secondNum;
+	    console.log(division);
+	};
 
-// // Create a function that accepts three arguments.
-//     // 1. First number
-//     // 2. Second number
-//     // 3. A function that performs an operation on them
-// // Return the value of the operation.
+}(operations || {}));
+
+// Create a function that accepts three arguments.
+    // 1. First number
+    // 2. Second number
+    // 3. A function that performs an operation on them
+// Return the value of the operation.
